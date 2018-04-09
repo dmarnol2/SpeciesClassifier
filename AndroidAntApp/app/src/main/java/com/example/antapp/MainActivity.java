45cd71164cd992a16d966157ef8776b7718b39d2
dmarnol2 <dmarnol2@asu.edu>
2018-04-09 00:26:36 -0400

    change splash page

--- AndroidAntApp/app/src/main/java/com/example/antapp/MainActivity.java
+++ AndroidAntApp/app/src/main/java/com/example/antapp/MainActivity.java
@@ -5,11 +5,41 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private final ImageView mImageView=null;
+    private Button logout;
+    private Button identifyButton;
+    private ImageButton file1Button;
+    private ImageButton file2Button;
+    private ImageButton file3Button;
+    private ImageButton camButton;
+    private String dlResults = "technomyrmex";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logout=(Button)findViewById(R.id.button);
+        identifyButton = (Button)findViewById(R.id.button2);
+        file1Button = (ImageButton)findViewById(R.id.imageButton);
+        //file2Button = (ImageButton)findViewById(R.id.imageButton2);
+        //file3Button = (ImageButton)findViewById(R.id.imageButton3);
+        camButton = (ImageButton)findViewById(R.id.imageButton4);
+        identifyButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent webIntent = new Intent(MainActivity.this, WebActivity.class);
+                webIntent.putExtra("ANT", "Technomyrmex");
+                startActivity(webIntent);
+            }// end onclick
+        });
+
+    }
 
     // intent to use camera
     private void dispatchTakePictureIntent() {
@@ -17,9 +47,11 @@ public class MainActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
 
-    @Override  // retrieves image taken from camera and displays it in ImageView
+
+}
+
+    /**@Override  // retrieves image taken from camera and displays it in ImageView
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
@@ -28,10 +60,6 @@ public class MainActivity extends AppCompatActivity {
             mImageView.setImageBitmap(imageBitmap);
         }
     }
+**/
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
